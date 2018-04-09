45935f192bea22ebc4d211a98291b2eacf248393
rakeshkukre <rakeshkukre777@gmail.com>
2018-04-09 15:24:03 +0530

    commit

--- Main.py
+++ Main.py
@@ -44,3 +44,13 @@ elif check == "Yo":
      print("Hello")
 else:
      print("It is actually equal to true")
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
